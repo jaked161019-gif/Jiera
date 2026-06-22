ef9385108ef2ae210ec2c687d4a336f4a5779891
jaked161019-gif <jaked161019@gmail.com>
2026-06-22 16:36:26 +0100

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,1 +1,14 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCj_4x8Yuql0U2OCEx6eHK_KAd8mJjqoL4",
+  authDomain: "jiera1.firebaseapp.com",
+  projectId: "jiera1",
+  storageBucket: "jiera1.firebasestorage.app",
+  messagingSenderId: "673366321859",
+  appId: "1:673366321859:web:35b3d4adf947e109249076",
+  measurementId: "G-F0GXDYCLC4"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const db = firebase.firestore();
 
